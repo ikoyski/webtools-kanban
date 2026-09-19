eb6696d4e898a97538d49c88dbca068a40647872
ikoyski <ikoyski@gmail.com>
2026-09-19 18:38:02 +0000

    added logout menu (not yet functional)

--- src/js/ui.js
+++ src/js/ui.js
@@ -7,6 +7,7 @@ export const UI = {
     aboutMenu: document.getElementById('about-menu'),
     exportMenu: document.getElementById('export-menu'),
     importMenu: document.getElementById('import-menu'),
+    logoutMenu: document.getElementById('logout-menu'),
     addColumnMenu: document.getElementById('add-column-menu'),
     importFile: document.getElementById('import-file'),
     themeDarkIcon: document.getElementById('theme-toggle-dark-icon'),
